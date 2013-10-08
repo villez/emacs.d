16b67f8a40f6d5a03fb5a13108391d984457f6cb
Ville Siltanen <ville.siltanen@iki.fi>
2013-10-09 00:23:15 +0300

    defun and key binding mods

--- vis-defun.el
+++ vis-defun.el
@@ -1,5 +1,6 @@
 ;; misc utility functions
 
+
 (defun visit-init-file ()
   "open init.el"
   (interactive)
@@ -10,15 +11,15 @@
   (interactive)
   (clipboard-kill-ring-save (point-min) (point-max)))
 
-(defun open-line-below()
-  "insert a new line below current, indent and put point at beginning"
+(defun open-line-below ()
+  "insert a new line below current and indent"
   (interactive)
   (unless (eolp)
     (end-of-line))
   (newline-and-indent))
 
-(defun open-line-above()
-  "insert a new line above current, indent and put point at beginning"
+(defun open-line-above ()
+  "insert a new line above current and indent"
   (interactive)
   (unless (bolp)
     (beginning-of-line))
@@ -26,13 +27,14 @@
   (forward-line -1)
   (indent-according-to-mode))
 
-(defun kill-this-line()
+;; shortcut for C-a C-k C-k
+(defun vis-kill-this-line ()
   "kill the whole line including newline no matter where the cursor is"
   (interactive)
   (beginning-of-line)
   (kill-whole-line))
 
-(defun dup-line()
+(defun vis-dup-line ()
   "duplicate the current line - no fanciness, just kill-yank-yank"
   (interactive)
   (move-beginning-of-line 1)
@@ -41,23 +43,38 @@
   (yank)
   (backward-char))
 
-(defun copy-this-line()
+;; shortcut for C-a C-spc C-n C-w
+(defun vis-copy-this-line ()
   "insert the current line with trailing newline into the kill ring"
   (interactive)
   (copy-region-as-kill (line-beginning-position) (line-beginning-position 2)))
 
-(defun kill-this-split()
+;; help I'm lost in this maze of split buffers
+(defun vis-kill-this-split ()
+  "kill the active buffer and remove split"
   (interactive)
   (delete-other-windows)
   (kill-buffer))
 
-(defun vis-frame-horizontal-bigger()
+;; a wide frame
+(defun vis-frame-horizontal-big ()
+  "make the current frame 150 characters wide"
   (interactive)
   (set-frame-width (selected-frame) 150))
 
-(defun vis-frame-horizontal-smaller()
+;; a narrower frame
+(defun vis-frame-horizontal-small ()
+  "make the current frame 110 characters wide"
   (interactive)
   (set-frame-width (selected-frame) 110))
 
+;; a simple workaround because normally yank doesn't work
+;; with C-u prefix arguments
+(defun vis-yank-n-times (arg)
+  "yank given number of times"
+  (interactive "*p")
+  (unless arg (setq arg 1))
+  (dotimes (n arg)
+    (yank)))
 
 (provide 'vis-defun)
--- vis-keybindings.el
+++ vis-keybindings.el
@@ -1,13 +1,19 @@
 ;; custom global key bindings
 
+;; shortcuts to built-in functions
 (global-set-key (kbd "C-x C-b") 'switch-to-buffer) ; I typo C-x b like this *all* the time
-(global-set-key (kbd "C-c a") 'copy-whole-buffer)
-(global-set-key (kbd "C-c c") 'copy-this-line)
 (global-set-key (kbd "C-c f") 'auto-fill-mode)
-(global-set-key (kbd "C-c d") 'text-insert-date)
-(global-set-key (kbd "C-c 2") 'dup-line)
-(global-set-key (kbd "C-c k") 'kill-this-line)
-(global-set-key (kbd "M-RET") 'open-line-below)
-(global-set-key (kbd "M-C-RET") 'open-line-above)
+(global-set-key (kbd "C-c n") 'comment-region)
+(global-set-key (kbd "C-c m") 'uncomment-region)
+
+;; shortcuts to own defuns - see vis-defun.el
+(global-set-key (kbd "C-c a") 'vis-copy-whole-buffer)
+(global-set-key (kbd "C-c c") 'vis-copy-this-line)
+(global-set-key (kbd "C-c d") 'vis-text-insert-date)
+(global-set-key (kbd "C-c 2") 'vis-dup-line)
+(global-set-key (kbd "C-c k") 'vis-kill-this-line)
+(global-set-key (kbd "C-c y") 'vis-yank-n-times)
+(global-set-key (kbd "M-RET") 'vis-open-line-below)
+(global-set-key (kbd "M-C-RET") 'vis-open-line-above)
 
 (provide 'vis-keybindings)
