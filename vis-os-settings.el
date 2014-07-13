b1ce267362ae9d1995cde93a7cce2ae70c78d98b
Ville Siltanen <ville.siltanen@iki.fi>
2014-07-13 20:26:00 +0300

    add explicit font setting for OS X

--- vis-os-settings.el
+++ vis-os-settings.el
@@ -3,6 +3,6 @@
   (setq mac-option-modifier nil)
   (setq mac-command-modifier 'meta)
   (setq x-select-enable-clipboard t)
-  )
+  (set-face-attribute 'default nil :family "Monaco" :height 120 :weight 'normal))
 
 (provide 'vis-os-settings)
