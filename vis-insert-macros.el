e98dd8832eb21f6fa8c3cbbfc089ff8afe3f4c84
Ville Siltanen <ville.siltanen@iki.fi>
2014-11-19 12:18:11 +0200

    added function vis-c-insert-include-guard to construct & insert a C/C++ include guard into the current buffer

--- vis-insert-macros.el
+++ vis-insert-macros.el
@@ -1,4 +1,7 @@
-;; text expander style macros
+;; text inserting macros
+
+
+;; inserting current date, time, or both
 
 (defun vis-text-insert-date()
   (interactive)
@@ -12,5 +15,27 @@
   (interactive)
   (insert (format-time-string "%Y-%m-%d %R")))
 
+
+;; inserting a C/C++ include guard of the form:
+;; [beginning-of-buffer]
+;; #ifndef FILE_BASENAME_H"
+;; #define FILE_BASENAME_H
+;; ...
+;; #endif
+;; [end of buffer]
+ 
+(defun vis-c-insert-include-guard()
+  (interactive)
+  (let ((guard-str
+         (concat
+          (replace-regexp-in-string "[.-]" "_"
+                                    (upcase (file-name-sans-extension (buffer-name))))
+          "_H")))
+    (save-excursion
+      (beginning-of-buffer)
+      (insert (concat "#ifndef " guard-str "\n"))
+      (insert (concat "#define " guard-str "\n"))
+      (end-of-buffer)
+      (insert "\n#endif\n"))))
 
 (provide 'vis-insert-macros)
